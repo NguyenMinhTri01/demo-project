2ffdf74aa286203fb2fefed46e5bddb7aa1dcf52
Nguyễn Minh Trí <17521156@ms.uit.edu.vn>
2021-03-09 23:18:07 +0700

    add view login and home page

---
+++ src/components/line-item.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="my-3 p-4 bg-white rounded box-shadow border-bottom">
+    <table class="table table-bordered">
+      <thead>
+        <tr class="bg-success">
+          <th colspan="4">
+            <div class="row">
+              <h3 class="col-6 text-left text-warning font-weight-bold">
+                LINE 8
+              </h3>
+              <h3 class="col-6 text-right text-danger font-weight-bold">
+                17/10/2016 - 08:54:43
+              </h3>
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="bg-primary">
+            <h4 class="font-weight-bold text-white">MODEL</h4>
+          </td>
+          <td>
+            <h4 class="text-danger font-weight-bold">A01</h4>
+          </td>
+          <td class="bg-primary">
+            <h4 class="font-weight-bold text-white">ID</h4>
+          </td>
+          <td><h4 class="text-danger font-weight-bold">AA111</h4></td>
+        </tr>
+        <tr class="font-weight-bold">
+          <td class="bg-gray">PLANT</td>
+          <td>1200</td>
+          <td class="bg-gray">TARGET</td>
+          <td>600</td>
+        </tr>
+        <tr class="font-weight-bold">
+          <td class="bg-gray">ACTUAL</td>
+          <td>597</td>
+          <td class="bg-gray">HOURS</td>
+          <td>8</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scope>
+.bg-gray {
+  background-color: #b7bcbf !important;
+}
+
+.border-bottom {
+  border-bottom: 4px solid #007bff !important;
+  border-bottom-width: 4px !important;
+  border-bottom-style: solid !important;
+  border-bottom-color: rgb(0, 123, 255) !important;
+}
+
+div.bg-white:hover {
+  background-color: #eef0f1 !important;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    <Topnavbar></Topnavbar>
+    <div class="container">
+      <LineItem></LineItem>
+      <LineItem></LineItem>
+      <LineItem></LineItem>
+    </div>
+    <div class="card-footer text-muted">FOOTER</div>
+  </div>
+</template>
+
+<script>
+import Topnavbar from "./../components/navbar-top";
+import LineItem from "./../components/line-item";
+export default {
+  components: {
+    Topnavbar,
+    LineItem,
+  },
+};
+</script>
+
+<style scope>
+</style>
